623725938e3e13454e0f2bc8cb069d0091f93778
lanjz <lanjz@mingyuanyun.com>
2018-07-31 14:25:53 +0800

    添加路由

---
+++ Demo/httpRouter/request.go
@@ -0,0 +1,181 @@
+package odserver
+
+import "net/http"
+
+type Request struct {
+	*http.Request
+	*HandlerObject
+}
+type FuncObject struct {
+	params []string
+	//对应编写的接口，IHandlerFunc是个空接口
+	f     IHandlerFunc
+	exist bool
+	*httpConfig
+}
+
+func NewFuncObject(f IHandlerFunc) FuncObject {
+	return FuncObject{
+		f:          f,
+		exist:      true,
+		httpConfig: &httpConfig{header: make(map[string]string)},
+	}
+}
+
+//方法函数映射，0代表GET方法下的接口
+type methodFuncs []FuncObject
+
+/**
+	关键struct，代表每个实体的请求
+ */
+type HandlerObject struct {
+	*Router
+	//对应占位符的参数
+	params []string
+	//对该请求的http配置
+	*httpConfig
+	//请求路径 即start+target的路径
+	path      string
+	startPath string
+	//方法函数映射
+	methodFuncs methodFuncs
+}
+
+//接口函数单位，即我们编写代码逻辑的函数
+type IHandlerFunc interface{}
+
+type IHandler interface {
+	Get(f IHandlerFunc) *HandlerObject
+	Post(f IHandlerFunc) *HandlerObject
+	Put(f IHandlerFunc) *HandlerObject
+	Delete(f IHandlerFunc) *HandlerObject
+}
+
+func NewHandlerObject(r *Router, startPath string) *HandlerObject {
+	return &HandlerObject{
+		params:      make([]string, paramsSize),
+		Router:      r,
+		startPath:   startPath,
+		methodFuncs: make([]FuncObject, TRACE),
+	}
+}
+
+func NewRequest(r *http.Request) Request {
+	return Request{Request: r}
+}
+
+func (ho *HandlerObject) Get(f IHandlerFunc) *HandlerObject {
+	if ho.methodFuncs[GET].exist {
+		panic("GetFunc has existed")
+	}
+	ho.methodFuncs[GET] = NewFuncObject(f)
+	return ho
+}
+
+func (ho *HandlerObject) Post(f IHandlerFunc) *HandlerObject {
+	if ho.methodFuncs[POST].exist {
+		panic("PostFunc has existed")
+	}
+	ho.methodFuncs[POST] = NewFuncObject(f)
+	return ho
+}
+
+func (ho *HandlerObject) Put(f IHandlerFunc) *HandlerObject {
+	if ho.methodFuncs[PUT].exist {
+		panic("PutFunc has existed")
+	}
+	ho.methodFuncs[PUT] = NewFuncObject(f)
+	return ho
+}
+func (ho *HandlerObject) Delete(f IHandlerFunc) *HandlerObject {
+	if ho.methodFuncs[DELETE].exist {
+		panic("DeleteFunc has existed")
+	}
+	ho.methodFuncs[PUT] = NewFuncObject(f)
+	return ho
+}
+
+func (ho *HandlerObject) Func(method int) (FuncObject, bool) {
+	switch method {
+	case GET:
+		return ho.getFunc()
+	case DELETE:
+		return ho.deleteFunc()
+	case PUT:
+		return ho.putFunc()
+	case POST:
+		return ho.postFunc()
+	case TRACE:
+		return ho.traceFunc()
+	case PATCH:
+		return ho.patchFunc()
+	case OPTIONS:
+		return ho.optionsFunc()
+	case HEAD:
+		return ho.headFunc()
+	case CONNECTIBNG:
+		return ho.connectingFunc()
+	}
+	return FuncObject{}, false
+
+}
+
+func (ho *HandlerObject) getFunc() (FuncObject, bool) {
+	if ho.methodFuncs[GET].exist {
+		ho.methodFuncs[GET].params = ho.params
+	}
+	return ho.methodFuncs[GET], ho.methodFuncs[GET].exist
+}
+func (ho *HandlerObject) postFunc() (FuncObject, bool) {
+	if ho.methodFuncs[POST].exist {
+		ho.methodFuncs[POST].params = ho.params
+	}
+	return ho.methodFuncs[POST], ho.methodFuncs[POST].exist
+}
+func (ho *HandlerObject) putFunc() (FuncObject, bool) {
+	if ho.methodFuncs[PUT].exist {
+		ho.methodFuncs[PUT].params = ho.params
+	}
+	return ho.methodFuncs[PUT], ho.methodFuncs[PUT].exist
+}
+
+func (ho *HandlerObject) deleteFunc() (FuncObject, bool) {
+	if ho.methodFuncs[DELETE].exist {
+		ho.methodFuncs[DELETE].params = ho.params
+	}
+	return ho.methodFuncs[DELETE], ho.methodFuncs[DELETE].exist
+}
+
+func (ho *HandlerObject) connectingFunc() (FuncObject, bool) {
+	if ho.methodFuncs[CONNECTIBNG].exist {
+		ho.methodFuncs[CONNECTIBNG].params = ho.params
+	}
+	return ho.methodFuncs[CONNECTIBNG], ho.methodFuncs[CONNECTIBNG].exist
+}
+func (ho *HandlerObject) headFunc() (FuncObject, bool) {
+	if ho.methodFuncs[HEAD].exist {
+		ho.methodFuncs[HEAD].params = ho.params
+	}
+	return ho.methodFuncs[HEAD], ho.methodFuncs[HEAD].exist
+}
+
+func (ho *HandlerObject) optionsFunc() (FuncObject, bool) {
+	if ho.methodFuncs[OPTIONS].exist {
+		ho.methodFuncs[OPTIONS].params = ho.params
+	}
+	return ho.methodFuncs[OPTIONS], ho.methodFuncs[OPTIONS].exist
+}
+
+func (ho *HandlerObject) patchFunc() (FuncObject, bool) {
+	if ho.methodFuncs[PATCH].exist {
+		ho.methodFuncs[PATCH].params = ho.params
+	}
+	return ho.methodFuncs[PATCH], ho.methodFuncs[PATCH].exist
+}
+
+func (ho *HandlerObject) traceFunc() (FuncObject, bool) {
+	if ho.methodFuncs[TRACE].exist {
+		ho.methodFuncs[TRACE].params = ho.params
+	}
+	return ho.methodFuncs[TRACE], ho.methodFuncs[TRACE].exist
+}
---
+++ Demo/httpRouter/router.go
@@ -0,0 +1,135 @@
+package odserver
+
+import (
+	"strings"
+	"regexp"
+	"time"
+)
+
+/**
+提供基本的路由功能，添加路由，查找路由
+ */
+const (
+	GET         = iota
+	POST
+	PUT
+	DELETE
+	CONNECTIBNG
+	HEAD
+	OPTIONS
+	PATCH
+	TRACE
+	paramsSize  = 6
+)
+
+var matcher PathMatcher = NewAntPathMatcher()
+
+type handler map[string]*HandlerObject
+type regexpMap map[*regexp.Regexp]*HandlerObject
+
+func NewRouter() *Router {
+	return &Router{
+		handler:   make(map[string]*HandlerObject),
+		regexpMap: make(map[*regexp.Regexp]*HandlerObject),
+	}
+}
+
+type Router struct {
+	handler
+	regexpMap
+}
+
+func (r *Router) Start(url string) *HandlerObject {
+	return NewHandlerObject(r, AddSlash(url))
+}
+
+func (ho *HandlerObject) And() *HandlerObject {
+	if ho.Router == nil || ho.startPath == "" {
+		panic("ho.Router is nil or startPath is unknown，maybe u should use Start()")
+	}
+	return NewHandlerObject(ho.Router, ho.startPath)
+}
+
+func (ho *HandlerObject) Target(url string) *HandlerObject {
+	//设置完整的路径
+	if ho.startPath == "/" {
+		ho.path = ho.startPath + DeleteSlash(url)
+	} else {
+		if strings.HasSuffix(ho.startPath, "/") {
+			url = DeleteSlash(url)
+		} else {
+			url = AddSlash(url)
+		}
+		ho.path = ho.startPath + url
+	}
+	//尝试将url转换成正则表达式，如果没有占位符，则转换不成功
+	pattern, ok := matcher.ToPattern(ho.path)
+	if ok {
+		ho.path = pattern
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			panic("error compile pattern:" + pattern)
+		}
+		ho.Router.regexpMap[re] = ho
+	} else {
+		ho.handler[ho.path] = ho
+	}
+	return ho
+}
+func AddSlash(s string) string {
+	if !strings.HasPrefix(s, "/") {
+		s = "/" + s
+	}
+	return s
+}
+
+func DeleteSlash(s string) string {
+	if strings.HasPrefix(s, "/") {
+		array := strings.SplitN(s, "/", 2)
+		s = array[1]
+	}
+	return s
+}
+
+//匹配路径
+func (r *Router) doUrlMapping(url string, method int) (FuncObject, bool) {
+	ch := make(chan *HandlerObject)
+	//精准匹配
+	go func() {
+		if ho, ok := r.handler[url]; ok {
+			ch <- ho
+		}
+	}()
+	//正则匹配
+	go func() {
+		for k, v := range r.regexpMap {
+			if k.MatchString(url) {
+				pathArray := strings.Split(url, "/")[1:]
+				regexpArray := strings.Split(k.String(), "/")[1:]
+				if len(pathArray) == len(regexpArray) {
+					//设置参数
+					paramsNum := 0
+					for i := 0; i < len(pathArray); i++ {
+						if matcher.IsPattern(regexpArray[i]) {
+							v.params[paramsNum] = pathArray[i]
+							paramsNum++
+						}
+					}
+					v.params = v.params[:paramsNum]
+				}
+				ch <- v
+			}
+		}
+	}()
+	select {
+	case ho := <-ch:
+		{
+			return ho.Func(method)
+		}
+	case <-time.After(2e6):
+		{
+			return FuncObject{}, false
+		}
+	}
+
+}
